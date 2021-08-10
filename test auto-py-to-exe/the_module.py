273dd8a4dd8be9bc89156038f9cad932740e6e7f
htnminh <nhatminh2106@gmail.com>
2021-08-10 13:25:29 +0700

    delete test auto-py-to-exe

--- test auto-py-to-exe/the_module.py
+++
@@ -1,5 +0,0 @@
-def the_sum_of(args):
-    return sum(args)
-
-if __name__ == '__main__':
-    print("Inside the_module where __name__ == '__main__'")
--- test auto-py-to-exe/the_script.py
+++
@@ -1,7 +0,0 @@
-import the_module
-
-f = open('the_input.txt', 'r')
-input_string = f.readline()
-lst = [int(x) for x in input_string.split()]
-print(the_module.the_sum_of(lst))
-input('Enter to continue')
